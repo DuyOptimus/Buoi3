c86b711395bc1a586b184c0818953c780ea78518
DuyOptimus <nguyenduy11196@gmail.com>
2017-10-13 22:00:32 +0700

    th

---
+++ Client/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        Socket client;
+        IPEndPoint ipe;
+        EndPoint remote;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            remote = (EndPoint)ipe;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "keo";
+            int a = 0;
+            byte[] send = BitConverter.GetBytes(a);
+            client.SendTo(send, remote);
+
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipe;
+            client.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bao";
+            int a = 0;
+            byte[] send = BitConverter.GetBytes(a);
+            client.SendTo(send, remote);
+
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipe;
+            client.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bua";
+            int a = 0;
+            byte[] send = BitConverter.GetBytes(a);
+            client.SendTo(send, remote);
+
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipe;
+            client.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            server.Bind(ipe);
+            Console.WriteLine("wating for a client..");
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint remote = (EndPoint)(sender);
+            byte[] data = new byte[4];
+            server.ReceiveFrom(data, ref remote);
+            int clientchoosen = BitConverter.ToInt32(data, 0);
+            Random rand = new Random(0);
+            int serverchoosen = rand.Next(0, 2);
+            if (clientchoosen==serverchoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Hoa");
+                server.SendTo(send, remote);
+            }
+            if (clientchoosen>serverchoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thang");
+                server.SendTo(send, remote);
+            }
+            if ((clientchoosen == 0) && (serverchoosen == 2))
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thang");
+                server.SendTo(send, remote);
+            }
+            if (clientchoosen < serverchoosen)
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, remote);
+            }
+            if ((clientchoosen == 2) && (serverchoosen == 0))
+            {
+                byte[] send = Encoding.ASCII.GetBytes("Thua");
+                server.SendTo(send, remote);
+            }
+
+        }
+    }
+}
